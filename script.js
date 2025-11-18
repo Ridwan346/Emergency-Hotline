7dbeb22396bbe48df39a05c1ef5ab46480942a02
Ridwan346 <ridwanhossain955@gmail.com>
2025-11-18 12:33:42 +0600

    third commit

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ document.getElementById("love").addEventListener("click",function(){
 })
 
 //card section
+let card_btn = document.getElementsByClassName("btn-card")
+console.log(card_btn) 
+for(let btn of card_btn)(
+    btn.addEventListener("click",function(){
+       let p1 = btn.parentNode.parentNode.childNodes[3].textContent
+       console.log(p1)
+       let p2 =btn.parentNode.parentNode.childNodes[7].textContent
+       console.log(p2)
+       alert(`${p1}
+        ${p2}`)
+        let coin= document.getElementById("coin").textContent
+        let int = parseInt(coin)
+        int = int - 20
+        document.getElementById("coin").innerText = int;
+    })
+)
