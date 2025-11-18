85bc86fdb657aacc17e1bf9d4c966fee82bd7407
Ridwan346 <ridwanhossain955@gmail.com>
2025-11-18 11:27:25 +0600

    sconde commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//nav love count
+document.getElementById("love").addEventListener("click",function(){
+    let a = document.getElementById("spn").textContent
+    let b = parseInt(a)
+    b++
+    document.getElementById("spn").textContent = b;
+})
+
+//card section
